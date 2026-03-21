ee405496a75938cbe35d399dada53c16fc66cdd7
Belkacem-Aitallak <belkacemaitallak@gmail.com>
2026-03-21 10:50:32 +0100

    Update static.ts2

--- server/static.ts
+++ server/static.ts
@@ -1,25 +1,21 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
-
-console.log("FORCE NEW DEPLOY");
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "../public");
+  const distPath = path.join(process.cwd(), "public");
+
+  console.log("Serving static from:", distPath);
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`
+      `Could not find the build directory: ${distPath}`
     );
   }
 
   app.use(express.static(distPath));
 
   app.use("/*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
