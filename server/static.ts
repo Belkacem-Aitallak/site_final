0612f71819b020c7c0261020cf3c42148b233170
Belkacem-Aitallak <belkacemaitallak@gmail.com>
2026-03-21 11:11:03 +0100

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -15,7 +15,7 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  app.use("*", (_req, res) => {
+  app.get("*", (_req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
